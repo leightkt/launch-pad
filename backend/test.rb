3319908c2c062d81ddac995f5b8c274bb6a99474
Kat Leight <leightkt@gmail.com>
2021-01-20 11:50:49 -0700

    seeded db with Planets api

--- backend/test.rb
+++ backend/test.rb
@@ -1,6 +1,7 @@
 require 'rest-client'
+require 'pry'
 require 'json'
-Planet.destroy_all
+# Planet.destroy_all
 
 response = RestClient.get 'https://api.le-systeme-solaire.net/rest/bodies'
 result = JSON.parse response
@@ -8,10 +9,13 @@ planets = result["bodies"].select do |body|
     body["isPlanet"]
 end
 
-planets.each do |planet|
+def create_planets 
+    planets.map |planet| do
     Planet.create(
         englishName: planet["englishName"],
         gravity: planet["gravity"],
         radius: planet["polarRadius"]
     )
+    end
 end
+
