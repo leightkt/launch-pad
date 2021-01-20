e38a6b70b2f373f7c0890afffd1134697af4eb62
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-01-20 13:45:36 -0700

    included image file path

---
+++ backend/db/migrate/20210120203222_add_image_to_planets.rb
@@ -0,0 +1,6 @@
+class AddImageToPlanets < ActiveRecord::Migration[6.1]
+  def change
+    add_column :planets, :image, :string
+
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_175906) do
+ActiveRecord::Schema.define(version: 2021_01_20_203222) do
 
   create_table "planets", force: :cascade do |t|
     t.string "englishName"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_20_175906) do
     t.boolean "has_water"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
