827309ea8a8a55e7e6b7f7ff07db7e557cf52ed3
Arielle Ross-Levy <ariellerosslevy@Arielles-MacBook-Air.local>
2021-01-20 13:05:48 -0700

    added random functions for remaining object attributes

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,10 +8,18 @@ planets = result["bodies"].select do |body|
     body["isPlanet"]
 end
 
+def has_water 
+    [true, false].sample
+end
+
 planets.each do |planet|
     Planet.create(
         englishName: planet["englishName"],
         gravity: planet["gravity"],
-        radius: planet["polarRadius"]
-    )
+        radius: planet["polarRadius"],
+        avg_home_price: rand(100..1000000000),
+        temperature: rand(-388..800),
+        distance_from_earch: rand(41..999),
+        has_water: has_water
+        )
 end
